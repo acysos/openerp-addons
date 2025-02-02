470036200c60e01e7c1fa1410b8766a0ad2fb77b
Ignacio Ibeas <ignacio@acysos.com>
2025-02-03 00:55:16 +0100

    Deprecated Navarra

--- l10n_es_aeat_line_crlf/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Ignacio Ibeas <ignacio@acysos.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import models
-from . import wizard
